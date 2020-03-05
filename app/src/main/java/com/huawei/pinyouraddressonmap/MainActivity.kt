6689badbecd99bf02a645601b467e124b7705a97
DESKTOP-J04IS85\Asmaa Amrawy <aalamrawy@overcoffeesolutions.com>
2020-03-05 16:56:34 +0200

    creating a map

--- app/src/main/java/com/huawei/pinyouraddressonmap/MainActivity.kt
+++ app/src/main/java/com/huawei/pinyouraddressonmap/MainActivity.kt
@@ -2,11 +2,102 @@ package com.huawei.pinyouraddressonmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.huawei.hms.maps.HuaweiMap
+import com.huawei.hms.maps.OnMapReadyCallback
+import android.Manifest.permission.INTERNET
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.Context
+import android.content.pm.PackageManager
+import com.huawei.hms.maps.MapView
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private val TAG = "MapViewDemoActivity"
+    //Huawei map
+    private var hMap: HuaweiMap? = null
+
+    private var mMapView: MapView? = null
+
+    private val REQUEST_CODE = 1
+
+    private val RUNTIME_PERMISSIONS = arrayOf<String>(
+        WRITE_EXTERNAL_STORAGE,
+        READ_EXTERNAL_STORAGE,
+        ACCESS_COARSE_LOCATION,
+        ACCESS_FINE_LOCATION,
+        INTERNET
+    )
+
+    private val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (!hasPermissions(this, RUNTIME_PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, RUNTIME_PERMISSIONS, REQUEST_CODE);
+        }
+        //get mapview instance
+        mMapView = findViewById(R.id.mapView) as MapView
+        var mapViewBundle: Bundle? = null
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY)
+        }
+        mMapView!!.onCreate(mapViewBundle)
+        //get map instance
+        mMapView!!.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: HuaweiMap?) {
+        Log.d(TAG, "onMapReady: ");
+        hMap = map;
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mMapView!!.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mMapView!!.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mMapView!!.onDestroy()
+    }
+
+    override fun onPause() {
+        mMapView!!.onPause()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mMapView!!.onResume()
+    }
+
+    private fun hasPermissions(context: Context, runtimePermissions: Array<String>): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && runtimePermissions != null) {
+            for (permission in runtimePermissions) {
+                if (ActivityCompat.checkSelfPermission(
+                        context,
+                        permission
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return false
+                }
+            }
+        }
+        return true
     }
 }
+
